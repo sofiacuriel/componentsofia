ae9ae55dc491b815b86b593d34979e8b5f216cec
Sofia <sofia.curiel@newhorizons.edu.do>
2021-11-16 18:51:33 +0000

    primer component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ButtonComponent } from './button/button.component';
+import { TableComponent } from './table/table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ButtonComponent,
+    TableComponent
   ],
   imports: [
     BrowserModule,
